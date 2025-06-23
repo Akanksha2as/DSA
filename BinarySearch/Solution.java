0c5693f2da2be284863a818a1e0f2eedb60b3c9a
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-06-23 15:41:23 +0530

    Add files via upload

---
+++ BinarySearch/Solution.java
@@ -0,0 +1,41 @@
+//  https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/1673573990/?envType=problem-list-v2&envId=binary-search
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        int[] result = searchRange(nums, target);
+        System.out.println("First and Last Position: [" + result[0] + ", " + result[1] + "]");
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+    private static int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1;
+        int bound = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                bound = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return bound;
+    }
+}
