f60e910d037e5f81895ef95931074f8097b88abb
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-07-04 12:29:18 +0530

    Add files via upload

---
+++ Two Pointer+Sorting/MinimumWindowSubstring.java
@@ -0,0 +1,56 @@
+//https://leetcode.com/problems/minimum-window-substring/submissions/1685850249/S
+import java.util.*;
+
+public class MinimumWindowSubstring {
+
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        int required = tMap.size();
+        int formed = 0;
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        int left = 0, right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            if (tMap.containsKey(c) && windowCounts.get(c).intValue() == tMap.get(c).intValue()) {
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
+
+                if (tMap.containsKey(leftChar) && windowCounts.get(leftChar) < tMap.get(leftChar)) {
+                    formed--;
+                }
+                left++;
+            }
+            right++;
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+
+    public static void main(String[] args) {
+        MinimumWindowSubstring solution = new MinimumWindowSubstring();
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println("Minimum Window: " + solution.minWindow(s, t));
+    }
+}
