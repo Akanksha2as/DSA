dc607a22957853b73214bc82b8ca1237c97bb065
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-07-10 19:39:07 +0530

    Add files via upload

---
+++ Hashing/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/submissions/1693221946/
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums)
+            set.add(num);
+
+        int longest = 0;
+
+        for (int num : set) {
+        
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest Consecutive Sequence Length: " + longestConsecutive(nums));
+    }
+}
