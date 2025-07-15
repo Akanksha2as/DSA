e14bf9ac24ab4ff8cf3625cfa0da0df2d2922bdc
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-07-15 15:46:13 +0530

    Add files via upload

---
+++ Stack + Queue/DecodeString.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/decode-string/submissions/1698650305/?envType=problem-list-v2&envId=stack
+import java.util.*;
+
+public class DecodeString {
+
+    public static String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                int currentK = countStack.pop();
+                for (int i = 0; i < currentK; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+
+        return currentString.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+        String result = decodeString(s);
+        System.out.println("Decoded String: " + result);
+    }
+}
