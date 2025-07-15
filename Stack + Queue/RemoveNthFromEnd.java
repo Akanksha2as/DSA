c2236a5b65a88fdd3c0926306f2efbd8bbb70d6b
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-07-15 16:17:28 +0530

    Add files via upload

---
+++ Stack + Queue/RemoveNthFromEnd.java
@@ -0,0 +1,57 @@
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/1698679590/?envType=problem-list-v2&envId=linked-list
+public class RemoveNthFromEnd {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        RemoveNthFromEnd solution = new RemoveNthFromEnd();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2;
+
+        ListNode result = solution.removeNthFromEnd(head, n);
+        printList(result);
+    }
+}
