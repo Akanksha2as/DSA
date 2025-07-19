213a8084038e70dffd61695647109c2eb468be36
Akanksha Singh <151874582+Akanksha2as@users.noreply.github.com>
2025-07-19 11:06:29 +0530

    Add files via upload

---
+++ Graph/GraphBFS.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class GraphBFS {
+
+    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(0);
+        visited[0] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            bfs.add(node);
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+
+        return bfs;
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(2).add(4);
+
+        ArrayList<Integer> bfsTraversal = bfsOfGraph(V, adj);
+
+        System.out.println("BFS Traversal of the graph: " + bfsTraversal);
+    }
+}
